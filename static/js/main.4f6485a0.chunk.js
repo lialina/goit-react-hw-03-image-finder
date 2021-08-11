2eb86ecbd269e53370c1e9461ffbbebc137bb18d
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-08-11 19:21:00 +0300

    Updates

--- static/js/main.4f6485a0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{12:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{className:"App"})};n(11);i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[12,1,2]]]);
-//# sourceMappingURL=main.4f6485a0.chunk.js.map
